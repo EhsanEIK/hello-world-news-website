3e953be5dda03a2f568dc29b7506a06eb6ecb7e1
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-02 23:20:21 +0600

    added news in html page by category_id

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const showCategories = categories => {
         const div = document.createElement('div');
         div.classList.add('m-3');
         div.innerHTML = `
-        <button onclick=loadNews(${category.category_id}) class="btn btn-outline-secondary">${category.category_name}</button>
+        <button onclick="loadNews(${category.category_id})" class="btn btn-outline-secondary">${category.category_name}</button>
         `;
         categoriesContainer.appendChild(div);
 
@@ -27,6 +27,49 @@ const loadNews = async id => {
     const data = await res.json();
     showNews(data.data);
 }
-const showNews = news => {
-    console.log(news);
+const showNews = newses => {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
+    newses.forEach(news => {
+        console.log(news);
+        const div = document.createElement('div');
+        div.classList.add('row', 'rounded-2', 'shadow', 'p-3', 'm-5');
+        div.innerHTML = `
+        <div class="col-lg-4">
+            <img src="${news.thumbnail_url ? news.thumbnail_url : 'Not Given'}">
+        </div>
+        <div class="col-lg-8">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4 class="fw-bold">${news.title ? news.title : 'Not Given'}</h4>
+                    <p>${news.details ? news.details.slice(0, 300) : 'Not Given'}...</p>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-5">
+                    <div class="row">
+                        <div class="col-lg-3 d-flex align-items-center">
+                            <img src="${news.author.img ? news.author.img : 'Not Given'}" class="img-fluid rounded-circle">
+                        </div>
+                        <div class="col-lg-9">
+                            <p class="fw-semibold">
+                            ${news.author.name ? news.author.name : 'Not Given'}
+                            </p>
+                            <small>${news.author.published_date ? news.author.published_date : 'Not Given'}</small>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <h6>
+                        <img src="./icons/view.png" style="width: 20px;"> ${news.total_view ? news.total_view : 'Not Given'}
+                    </h6>
+                </div>
+                <div class="col-lg-2">
+                <button onclick="" class="btn btn-primary">Details</button>
+                </div>
+            </div>
+        </div>
+        `;
+        newsContainer.appendChild(div);
+    });
 }
